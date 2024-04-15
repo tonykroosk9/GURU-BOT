4829387932e8fae9099e97de13b5e735fb47532e
tonykroosk9 <167127910+tonykroosk9@users.noreply.github.com>
2024-04-15 22:04:11 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['254748840328', 'TYLOR', true],
+  ['254748840328', 'KNINE', true], 
   [''] 
 ] //Number of owners
 
